21132318e19b10e30df304ad754d17615108e28e
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2017-09-04 22:53:44 +0530

    Create sak21.c

---
+++ sak21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter an int");
+    scanf("%d",&n);
+
+    for(i=1; i<=10; ++i)
+    {
+        printf("%d * %d = %d", n, i, n*i);
+    }
+    
+}
