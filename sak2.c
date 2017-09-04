b879f8a88b1a29efc46de806523876f334553985
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2017-09-04 20:54:22 +0530

    Create sak2.c

---
+++ sak2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int no;
+
+    printf("Enter an integer: ");
+    scanf("%d", &no);
+
+ 
+   if(no % 2 == 0)
+        printf("%d is even", no);
+    else
+        printf("%d is odd", no);
+
+    return 0;
+}
