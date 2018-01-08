887aa6a2ebac7b21a5dab01e30639db43f76d07d
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-01-08 11:19:23 +0000

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char str[100];
+int countdigits,countspecialcharacters,countspace;
+int counter;
+countdigits=countspecialcharacters=countspace=0;
+printf("hello world.123')
+
+  
