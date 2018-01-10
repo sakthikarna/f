9af0b3fbf545694b206d54f734c814f142ea1d4d
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-01-10 11:10:52 +0000

    Update 36.c

--- 36.c
+++ 36.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int countspace,countspecialcharacters,countalphabet;
+  int double,i,j,n;
+count=0;
+ printf("hello world.123");
+ for(i=0;i<=n;i++)
+ {
+  for(i=0;i<=9;i++)
+  {
+  
+  
