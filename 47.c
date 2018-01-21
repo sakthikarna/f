b71952fdad9e79f113065a552e191394c813ec24
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-01-21 14:30:34 +0530

    Update 47.c

--- 47.c
+++ 47.c
@@ -1,1 +1,23 @@
-#include<stdio.h>
+int find_max(int arr[],int size){
+  int i;
+  int max = -1;
+  for(i=0;i<size;i++){ if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+int find_min(int arr[],int size){
+  int i;
+  int min = arr[0];
+  for(i=1;i<size;i++){
+    if(arr[i]<min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+int main(){
+  int i,total;
+  printf("Enter total no of elements : ");
+  scanf("%d",&total);
