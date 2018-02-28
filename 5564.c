93be433918fb7b7d9657dbc3f9c759678c3fb3ab
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-02-28 16:36:32 +0530

    Create 5564.c

---
+++ 5564.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,k,n;
+	char a[100],b[100];
+	scanf("%s",&a);
+	scanf("%s",&b);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==b[i])
+		{
+			printf("%c",a[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+	}
