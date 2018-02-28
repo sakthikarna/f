c3e2ffb884f8cb228521806be3d716dadf67cb09
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-02-28 16:53:59 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+
+    int n,i,c=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        printf("prime");
+
+    }
+    else
+    {
+        printf("not prime");
+    }
+}
