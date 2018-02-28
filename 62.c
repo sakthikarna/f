7f11af348a8fe7c230eec1e4eeab1524c6d8d1a8
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-02-28 15:41:46 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char s[10],i;
+    printf("enter the string");
+    scanf("%s",&s);
+    if(s[i]=='0'||'1')
+    {
+        i++;
+        printf("yes\n");
+    }
+    else
+    {
+        i++;
+        printf("no\n");
+    }
+}
