76be796f7c73267fc8a5899577539be7912510d1
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-02-28 16:17:57 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,a;
+    printf("enter the no");
+    scanf("%d %d",&n,&m);
+    a=n+m;
+    if(a%2==0)
+    {
+        printf("the no is even");
+    }
+    else
+    {
+        printf("the no is odd");
+            
+    }
+    
+        
+    }
+    
