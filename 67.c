ed841962d1f5929bc84388622662b93841e1e5fb
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-03-02 15:48:16 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the no");
+    scanf("%d",&n);
+    i=(n%7);
+    if(i==0)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    
+}
