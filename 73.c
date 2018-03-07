b085be8b5a297ef220f8b8861e5231d4f8e9ce7b
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-03-07 16:39:06 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,l,r;
+    printf("enter the no:");
+    scanf("%d %d %d",&n,&l,&r);
+    if(n>l)
+    {
+        printf("yes");
+    }
+   else
+    {
+        printf("no");
+    }
+}
