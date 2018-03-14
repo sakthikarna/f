3613e91425cdac0060ef0e94946b2c9a4c8bf2bd
sakthikarna <31514043+sakthikarna@users.noreply.github.com>
2018-03-14 16:14:13 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,m;
+    printf("enter the no:");
+    scanf("%d%d",&n,&i);
+    m=n*i;
+    if(n==i)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
